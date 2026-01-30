625cc80126879fe45fae63230d9d2c72690d067c
kenwanyama <kenwanyama.email@gmail.com>
2026-01-30 14:03:18 -0700

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -6,7 +6,7 @@ from fastapi.responses import JSONResponse
 
 from backend.storage.database import Article, SessionLocal
 from backend.app.scheduler import start_scheduler
-from backend.app.nlp_loader import nlp_loader
+from backend.nlp.nlp_loader import nlp_loader
 
 
 @asynccontextmanager
